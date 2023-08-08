2eb1ee2bfba5af4010330f370181501a3503f4e8
TomSmaj <thomas.smajstrla@gmail.com>
2023-08-08 11:01:53 -0500

    created project card

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,38 @@
 import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import "./Project.css";
 
 class Project extends Component {
-  constructor() {
-    super();
-    this.state = {
+    constructor() {
+        super();
+        this.state = {
 
+        }
+    };
+
+    componentDidMount = () => {
+
+    };
+
+    render() {
+        return (
+            <div className="project">
+                <Card style={{ width: '36rem' }}>
+                    <Card.Img variant="top" src={this.props.pic} />
+                    <Card.Body>
+                        <Card.Title>{this.props.name}</Card.Title>
+                        <Card.Text>
+                        {this.props.description.map(line => {
+                            return (<li className="card-text">{line}</li>)
+                        })}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
     }
-  };
-
-  componentDidMount = () => {
-
-  };
-
-  render() {
-    return (
-      <div className="project">
-        project
-      </div>
-    );
-  }
 }
 
 export default Project;
