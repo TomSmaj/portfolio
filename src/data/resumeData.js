1245583b918e6d15fe6600f16d818f8c703c39b7
TomSmaj <thomas.smajstrla@gmail.com>
2023-08-29 12:31:49 -0500

    working on resume page

---
+++ src/data/resumeData.js
@@ -0,0 +1,44 @@
+exports.employmentData = [
+    {
+        "title": "Application Support Engineer (Level 3)",
+        "company": "Q2",
+        "website": "https://www.q2.com/",
+        "dates": "08/2021 - Present",
+        "description": ["Act as the final line of support that receives issues from clients using Q2 bank account opening software", "Troubleshoot frontend (Javascript) and backend (Java) of account opening software, often using locally running instance of the application", "Identify and document bugs/code improvements needed and create detailed Jira tickets for the development team to take action on, with clear indication of what exact resolution is needed", "Manage client infrastructure in AWS (EC2, RDS, S3, Route 53, CloudWatch)", "Work with containerized client environments in Nomad as well as Docker", "Perform frequent MySQL querying to identify trends/issues affecting clients", "Work directly with customers over phone to address issues", "Personally write documentation as new undocumented knowledge arises", "Work with multiple vendor integrations and APIs utilized by Q2 software (Financial Core middleware, KBA APIs, Decisioning APIs, Funding APIs, etc.)", "Use Postman to recreate and diagnose issues arising with Q2 APIs"]
+    },
+    {
+        "title": "Google Maps API Developer Analyst",
+        "company": "Telus International (Google Contractor)",
+        "website": "https://www.telusinternational.com/",
+        "dates": "08/2018 - 08/2021",
+        "description": ["Led project that developed a chrome extension that tracks client side requests being made by a customer’s Google Maps implementation. Unit tested and committed to Google code repo.", "Provided technical support for Google Maps API users through phone, chat, and email", "Gained extensive experience troubleshooting code issues using browser developer console and network tools", "Reproduced user issues, diagnosed and debugged error codes, filed bug reports, and escalated issues as needed to higher level engineers", "Troubleshot issues that arose across numerous different platforms (web applications, desktop applications, Android, iOS, etc.)", "Updated internal and external knowledge base articles and documentation to assist both customers and colleagues", "Presented 2 years in a row at Google Maps Platform annual internal business review"]
+    },
+    {
+        "title": "Systems Engineer",
+        "company": "Toyota Tsusho Network Integration",
+        "website": "",
+        "dates": "08/2016 – 05/2017",
+        "description": ["Improved general user experience of employees by implementing SSO (Single Sign On) through Active Directory Services and Azure AD Connect","Provided support resolving technical issues (software, connectivity issues, problems printing, etc.), while stationed at client site", "Regularly performed System Administrative functions in Active Directory", "In charge of imaging and setting up hardware for new employees as well as clients’ new employees", "Gained thorough knowledge and experience with Windows Server 2012,  Windows Enterprise (7, 8, & 10), Mac OS X, and Ubuntu 16", "Used Powershell to perform actions in O365 such as assigning licenses, retrieving user login activity, and retrieving user group membership information"]
+    }
+]
+
+exports.skillsData = [
+    {
+        "category": "Programming Languages",
+        "data": [["React", "Node.js", "Express", "MySQL"], ["JavaScript", "HTML", "CSS", "Bootstrap", "Material UI", "MongoDB"], ["Python", "Java"]]
+    },
+    {
+        "category": "Cloud Services",
+        "data": [["AWS:", "EC2", "RDS", "S3", "CloudFront", "Amazon Simple Email Service", "Route 53", "Cloud Watch"]]
+    },
+    {
+        "category": "Software",
+        "data": [["Git", "Docker", "Postman", "OpenSSL", "Libreswan VPN"], ["Splunk", "Jira", "Confluence", "Bitbucket"]]
+    }
+]
+
+
+
+
+
+
